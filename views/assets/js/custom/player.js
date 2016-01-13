1d8f988e93ce9a8cb763a618b71dbc082dfa4f76
Leonid Fenko <fenok2112@gmail.com>
2016-01-13 07:33:37 +0300

    Reworked player.js (added waiting state)

--- views/assets/js/custom/player.js
+++ views/assets/js/custom/player.js
@@ -29,8 +29,7 @@ player.stateController = {};
 
 player.stateController.currentState =
 {
-	name : 'pause', // or 'play'
-	mode : 'instant', //or 'delayed'
+	name : 'pause', // or 'play' or 'waiting'
 	timestamp : -1, // bounded to playerTime AND is transition timestamp
 	playerTime : 0 // bounded to timestamp
 };
@@ -42,7 +41,7 @@ player.stateController.latestResponseTimestamp = -1;
 //delay (ms) to be applied before actual play/pause/(seek?) to prevent possible microdesync
 //it is about to change
 //TODO: may be calculated depending on actual latency in future
-player.stateController.magicDelay = 500; //200ms
+player.stateController.magicDelay = 200; //200ms
 
 //Maximum difference (ms) between currentTimes of players when they are considered to be synced 
 player.stateController.desyncInterval = 100; //100ms
@@ -51,39 +50,49 @@ player.stateController.delayedPlayPauseTimeout = null;
 
 player.stateController.syncTime = function( state )
 {
-	var offset = state.mode === 'delayed' ? player.stateController.magicDelay : 0;
-	offset += state.timestamp - currentTimestamp();
+	var offset = player.stateController.magicDelay + state.timestamp - currentTimestamp();
 	var supposedTime = -1;
 	
 	//why not switch/case? testing speed o.o
 	if (state.name === 'play')
 	{
-		if (offset > 0)
+		if (player.stateController.currentState.name !== 'play')
 		{
-			supposedTime = state.playerTime;
+			if (offset > 0)
+			{
+				supposedTime = state.playerTime;
+			}
+			else
+			{
+				supposedTime = state.playerTime - offset;
+			}
 		}
 		else
 		{
 			supposedTime = state.playerTime - offset;
 		}
 	}
-	else // if (state.name === 'pause')
+	else if (state.name === 'pause')
 	{
-		if (offset > 0) 
+		if (player.stateController.currentState.name === 'play')
 		{
-			supposedTime = state.playerTime + player.stateController.magicDelay - offset;
-		}
-		else
-		{
-			if (state.mode === 'delayed')
+			if (offset > 0) 
 			{
-				supposedTime = state.playerTime + player.stateController.magicDelay;
+				supposedTime = state.playerTime + player.stateController.magicDelay - offset;
 			}
 			else
 			{
-				supposedTime = state.playerTime;
+				supposedTime = state.playerTime + player.stateController.magicDelay;
 			}
 		}
+		else
+		{
+			supposedTime = state.playerTime;
+		}
+	}
+	else //state.name === 'waiting'
+	{
+		supposedTime = state.playerTime;
 	}
 	
 	var diff = Math.abs(supposedTime - player.elements.video.currentTime * 1000);
@@ -115,8 +124,6 @@ player.stateController.sendCurrentState = function ()
 
 player.stateController.updateCurrentState = function( state )
 {
-	outputSystemMessage(state.mode+"_"+state.name);
-	
 	/* TODO: Can we use that (optimization)?
 	
 	if (state.timestamp < player.stateController.currentState.timestamp)
@@ -125,24 +132,26 @@ player.stateController.updateCurrentState = function( state )
 	*/
 
 	//crutch to prevent cascade instant pause updates on 'waiting'
-	if(state.waiting && player.stateController.currentState.waiting)
+	if(state.name === 'waiting' && player.stateController.currentState.name === 'waiting')
 	{
 		player.stateController.currentState.timestamp = state.timestamp;
 		return;
 	}
 	
+	outputSystemMessage(state.name);
+	
 	//New state is being applied, so we need to clear the timeout to prevent unexpected changes
 	clearTimeout(player.stateController.delayedPlayPauseTimeout);
 	
 	//offset is used as delay before actual play/pause
-	var offset = state.mode === 'delayed' ? player.stateController.magicDelay : 0;
-	offset +=state.name === 'pause' ? player.stateController.syncTime(state) : 0 - player.stateController.syncTime(state);
+	var offset = player.stateController.magicDelay;
+	offset += state.name !== 'play' ? player.stateController.syncTime(state) : 0 - player.stateController.syncTime(state);
 	offset += state.timestamp - currentTimestamp();
 	
 	//why not switch/case? testing speed o.o
 	if (state.name === 'play')
 	{
-		if (offset > 0) // delay before play -- can occur ONLY if mode === 'delayed'
+		if (offset > 0) // delay before play
 		{
 			player.stateController.delayedPlayPauseTimeout = setTimeout(function()
 			{
@@ -150,27 +159,40 @@ player.stateController.updateCurrentState = function( state )
 				player.elements.playPauseButton.switchToPause();
 			}, offset);
 		}
-		else // magic delay was less than latency OR mode === 'instant'
+		else // magic delay was less than latency
 		{
 			player.playbackController.play();
 			player.elements.playPauseButton.switchToPause();
 		}
 	}
-	else //state.name === 'pause'
+	else if (state.name === 'pause')
 	{
-		if (offset > 0) // delay before pause -- can occur ONLY if mode === 'delayed'
+		if (player.stateController.currentState.name !== 'play')
 		{
-			player.stateController.delayedPlayPauseTimeout = setTimeout(function()
+			player.playbackController.play();
+			player.playbackController.pause();
+		}
+		else
+		{
+			if (offset > 0) // delay before pause
+			{
+				player.stateController.delayedPlayPauseTimeout = setTimeout(function()
+				{
+					player.playbackController.pause();
+					player.elements.playPauseButton.switchToPlay();
+				}, offset);
+			}
+			else // magic delay was less than latency
 			{
 				player.playbackController.pause();
 				player.elements.playPauseButton.switchToPlay();
-			}, offset);
+			}
 		}
-		else // magic delay was less than latency OR mode === 'instant'
-		{
-			player.playbackController.pause();
-			player.elements.playPauseButton.switchToPlay();
-		}
+	}
+	else //state.name === 'waiting'
+	{
+		//can occur ONLY after seeking, assuming playerTimes are in sync
+		player.playbackController.pause();
 	}
 
 	player.stateController.currentState = state;
@@ -183,7 +205,6 @@ player.stateController.onPlayerPlay = function( playerTime )
 		player.stateController.updateCurrentState(
 		{
 			name : "play",
-			mode : "delayed",
 			timestamp : currentTimestamp(),
 			playerTime : playerTime
 		} );
@@ -198,7 +219,6 @@ player.stateController.onPlayerPause = function(playerTime)
 		player.stateController.updateCurrentState(
 		{
 			name : "pause",
-			mode : "delayed",
 			timestamp : currentTimestamp(),
 			playerTime : playerTime
 		} );
@@ -210,8 +230,7 @@ player.stateController.onPlayerSeek = function( playerTime )
 {
 	player.stateController.updateCurrentState(
 	{
-		name : player.stateController.currentState.name,
-		mode : player.stateController.currentState.name === 'play' ? 'delayed' : 'instant',
+		name : player.stateController.currentState.name === 'play' ? 'play' : 'pause', //seeking switches 'waiting' to 'pause'
 		timestamp : currentTimestamp(),
 		playerTime : playerTime
 	});
@@ -274,19 +293,17 @@ player.elements.video.addEventListener('waiting', function()
 		return;
 	*/
 	
-	if (player.stateController.currentState.name !== 'pause' || player.stateController.currentState.mode !== 'instant')
+	if (player.stateController.currentState.waiting === undefined)
 	{
+		player.elements.playPauseButton.switchToWaiting();
 		player.stateController.updateCurrentState(
 			{
-				name : "pause",
-				mode : "instant",
-				waiting : "waiting",
+				name : "waiting",
 				timestamp : currentTimestamp(),
 				playerTime : player.stateController.currentState.playerTime
 			} );
 		player.stateController.sendCurrentState();
 	}
-	player.elements.playPauseButton.switchToWaiting();
 });
 
 player.elements.video.addEventListener('canplay', function()
