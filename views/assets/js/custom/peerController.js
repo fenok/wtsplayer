f17736a1c3fb1a41ffd9ea54cceec2adb71c0bc3
Leonid Fenko <fenok2112@gmail.com>
2016-03-12 23:07:49 +0300

    Added a bunch of ICE servers

--- views/assets/js/custom/peerController.js
+++ views/assets/js/custom/peerController.js
@@ -13,7 +13,7 @@ wtsplayer.peerController = function()
 		elementsController : {
 			outputSystemMessage  : null,
 			onRecieved           : null,
-			onGotRoomStatus    : null,
+			onGotRoomStatus      : null,
 			getDataSource        : null,
 			onPeerDeleted        : null,
 			onJoinedRoom         : null,
@@ -143,13 +143,51 @@ wtsplayer.peerController = function()
 		_peer = new Peer( '',
 			{
 				host   : location.hostname,
-				port   : location.protocol === 'https:' ? 8443 : 8000 ,
+				port   : location.protocol === 'https:' ? 8443 : 8000,
 				path   : '/peerjs',
 				secure : location.protocol === 'https:',
+				config : {
+					'iceServers' : [
+						{ url : 'stun:stun01.sipphone.com' },
+						{ url : 'stun:stun.ekiga.net' },
+						{ url : 'stun:stun.fwdnet.net' },
+						{ url : 'stun:stun.ideasip.com' },
+						{ url : 'stun:stun.iptel.org' },
+						{ url : 'stun:stun.rixtelecom.se' },
+						{ url : 'stun:stun.schlund.de' },
+						{ url : 'stun:stun.l.google.com:19302' },
+						{ url : 'stun:stun1.l.google.com:19302' },
+						{ url : 'stun:stun2.l.google.com:19302' },
+						{ url : 'stun:stun3.l.google.com:19302' },
+						{ url : 'stun:stun4.l.google.com:19302' },
+						{ url : 'stun:stunserver.org' },
+						{ url : 'stun:stun.softjoys.com' },
+						{ url : 'stun:stun.voiparound.com' },
+						{ url : 'stun:stun.voipbuster.com' },
+						{ url : 'stun:stun.voipstunt.com' },
+						{ url : 'stun:stun.voxgratia.org' },
+						{ url : 'stun:stun.xten.com' },
+						{
+							url        : 'turn:numb.viagenie.ca',
+							credential : 'muazkh',
+							username   : 'webrtc@live.com'
+						},
+						{
+							url        : 'turn:192.158.29.39:3478?transport=udp',
+							credential : 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+							username   : '28224511:1379330808'
+						},
+						{
+							url        : 'turn:192.158.29.39:3478?transport=tcp',
+							credential : 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+							username   : '28224511:1379330808'
+						}
+					]
+				},
 				debug  : 1
 			} );
 
-		console.log(location.hostname, location.port, location.protocol);
+		console.log( location.hostname, location.port, location.protocol );
 
 		_peer.on( 'open', function( id )
 		{
