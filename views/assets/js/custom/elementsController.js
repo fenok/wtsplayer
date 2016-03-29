0cb8a9d4dc2bc1f030576bac70e978ce18187059
Leonid Fenko <fenok2112@gmail.com>
2016-03-30 00:57:28 +0300

    GIDDY UP GIDDY UP GIDDY UP

--- views/assets/js/custom/elementsController.js
+++ views/assets/js/custom/elementsController.js
@@ -1887,7 +1887,7 @@ wtsplayer.elementsController = function()
 
 	function clear()
 	{
-		_inputLink.value             = "magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4";
+		_inputLink.value             = "https://youtu.be/-npBowNYDfg";
 		_localURL.value              = "";
 		_peersSrc.innerHTML          = "";
 		_muteVideo                   = false;
